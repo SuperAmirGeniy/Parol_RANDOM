02aa2c901d9a56a659cf3777972f591c1e3d02a9
SuperAmirGeniy <143881085+SuperAmirGeniy@users.noreply.github.com>
2023-09-09 15:16:45 +0300

    1 version

---
+++ 1.py
@@ -0,0 +1,10 @@
+import random
+rp = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+dlinaparol = int(input('введи длину пароля'))
+
+parol = ''
+
+for i  in range(pas):
+    parol += random.choise(rp)
+print(parol)
